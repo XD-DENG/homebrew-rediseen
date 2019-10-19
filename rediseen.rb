2c67ec4bf391fc8152026ecaf8c7f047459aa430
Xiaodong <xd_deng@hotmail.com>
2019-10-19 09:59:48 +0800

    Update rediseen.rb

--- rediseen.rb
+++ rediseen.rb
@@ -14,6 +14,20 @@ class Rediseen < Formula
   end
 
   test do
-    system "#{bin}/#{name}", "version"
+    assert_equal "1.0.0", shell_output("#{bin}/#{name} version").strip
+    assert_match "[ERROR] No valid Redis URI is provided",
+                 shell_output("#{bin}/#{name} start")
+    assert_match "REDISEEN_DB_EXPOSED is not configured",
+                 shell_output("export REDISEEN_REDIS_URI=redis://:@localhost:6379;" \
+                              "#{bin}/#{name} start")
+    assert_match "[ERROR] You have not specified any key pattern to allow being accessed",
+                 shell_output("export REDISEEN_REDIS_URI=redis://:@localhost:6379;" \
+                              "export REDISEEN_DB_EXPOSED=0;" \
+                              "#{bin}/#{name} start")
+    assert_match "[ERROR] Initial talking to Redis failed. Please check the URI provided.",
+                 shell_output("export REDISEEN_REDIS_URI=redis://:@localhost:6379;" \
+                              "export REDISEEN_DB_EXPOSED=0;" \
+                              "export REDISEEN_KEY_PATTERN_EXPOSE_ALL=true;" \
+                              "#{bin}/#{name} start")
   end
 end

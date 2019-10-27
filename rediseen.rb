739fbf0a72932765968e93ad23acd361be6e38d2
Xiaodong <xd_deng@hotmail.com>
2019-10-27 11:36:59 +0800

    release 1.1.0

--- rediseen.rb
+++ rediseen.rb
@@ -1,8 +1,8 @@
 class Rediseen < Formula
   desc "Start REST API service for Redis without writing a single line of code"
   homepage "https://github.com/XD-DENG/rediseen"
-  url "https://github.com/XD-DENG/rediseen/archive/1.0.2.tar.gz"
-  sha256 "5ba62edfed2a0aa8221f09800fb16d65bb51aeb38b544951eb16e83afad6239f"
+  url "https://github.com/XD-DENG/rediseen/archive/1.1.0.tar.gz"
+  sha256 "c289c8199130e384707d8192df23b08096f09ab33043db45159d327cdea1c157"
 
   head do
     url "https://github.com/XD-DENG/rediseen.git"
@@ -14,7 +14,7 @@ class Rediseen < Formula
   end
 
   test do
-    assert_equal "1.0.2", shell_output("#{bin}/#{name} version").strip
+    assert_match "1.1.0", shell_output("#{bin}/#{name} version").strip
     assert_match "[ERROR] No valid Redis URI is provided",
                  shell_output("#{bin}/#{name} start")
     assert_match "REDISEEN_DB_EXPOSED is not configured",
@@ -25,7 +25,7 @@ class Rediseen < Formula
                               "export REDISEEN_DB_EXPOSED=0;" \
                               "#{bin}/#{name} start")
     assert_match "[ERROR] Initial talking to Redis failed. Please check the URI provided.",
-                 shell_output("export REDISEEN_REDIS_URI=redis://:@localhost:6379;" \
+                 shell_output("export REDISEEN_REDIS_URI=redis://:@localhost:8888;" \
                               "export REDISEEN_DB_EXPOSED=0;" \
                               "export REDISEEN_KEY_PATTERN_EXPOSE_ALL=true;" \
                               "#{bin}/#{name} start")

e33ba2caf3909b29c110da1d49cea99710d952ec
Xiaodong <xd_deng@hotmail.com>
2019-11-04 23:56:39 +0800

    Version 1.2.0

--- rediseen.rb
+++ rediseen.rb
@@ -1,8 +1,8 @@
 class Rediseen < Formula
   desc "Start REST API service for Redis without writing a single line of code"
   homepage "https://github.com/XD-DENG/rediseen"
-  url "https://github.com/XD-DENG/rediseen/archive/1.1.1.tar.gz"
-  sha256 "542f0c27ff0ee21b6976cd9302b2ffb180a3f5b1a683a136dfcfdf7969c72556"
+  url "https://github.com/XD-DENG/rediseen/archive/1.2.0.tar.gz"
+  sha256 "05fd584f04c888974adb9c812c92d4795d9298b73ce45068f6b6500ea2f8f4a2"
 
   head do
     url "https://github.com/XD-DENG/rediseen.git"
@@ -14,7 +14,7 @@ class Rediseen < Formula
   end
 
   test do
-    assert_match "1.1.1", shell_output("#{bin}/#{name} version").strip
+    assert_match "1.2.0", shell_output("#{bin}/#{name} version").strip
     assert_match "[ERROR] No valid Redis URI is provided",
                  shell_output("#{bin}/#{name} start")
     assert_match "REDISEEN_DB_EXPOSED is not configured",
